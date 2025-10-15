54fcc498a868e94c84ce87d6a6455c1deae48a59
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-10-15 17:09:35 +0100

    Adding Service Times

---
+++ ChurchSermonsMetaData/Models/Services.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchSermonsMetaData.Models
+{
+    internal static  class Services
+    {
+        public static string[] ServiceTimes = ["Morning Service", "Evening Service", "Midweek Meeting", "Other"];
+    }
+}
