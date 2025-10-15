70383802ba23a2d68f2271cb503c0fc8c94a95cf
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-10-15 17:09:53 +0100

    adding Speaker Model

---
+++ ChurchSermonsMetaData/Models/Speakers.cs
@@ -0,0 +1,8 @@
+﻿namespace ChurchSermonsMetaData.Models;
+
+public class Speakers
+{
+    public List<string> ElderList { get; set; } = [];
+
+    public List<string> PreviousSpeakers { get; set; } = [];
+}
