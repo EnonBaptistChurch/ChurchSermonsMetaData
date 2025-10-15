6c0b9cc155cf000eb58ea1b8369204087433bf82
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-10-15 17:09:18 +0100

    adding SermonInfo Model

---
+++ ChurchSermonsMetaData/Models/SermonInfo.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ChurchSermonsMetaData.Models;
+
+internal record SermonInfo
+{
+    public string Title { get; set; } = string.Empty;
+    public string Speaker { get; set; } = string.Empty;
+    public string Series { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Service { get; set; } = string.Empty;
+    public List<string> BiblePassage { get; set; } = [];
+}
