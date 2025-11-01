09b20bc9302de1326d72896b14e726fc0a10b3a0
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-11-01 17:04:24 +0000

    Sermon Service Date

---
+++ ChurchSermonsMetaData/Models/SermonServiceAndDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchSermonsMetaData.Models
+{
+    internal record SermonServiceAndDate
+    {
+        public DateTime? Date { get; set; }
+        public string Service {  get; set; } = string.Empty;
+    }
+}
