6d57b8d0ae1a3946854d25c8e9050fd0bfcc5610
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-11-01 17:02:12 +0000

    Add SeriesStorage and SpeakerStorage classes
    Introduce SeriesStorage and SpeakerStorage classes for handling
    JSON serialization of series and speaker data in the
    ChurchSermonsMetaData.Data namespace. Implement methods for
    adding, loading, and saving data, with pretty-printed JSON
    output. Define static properties for file paths and handle
    file existence checks to initialize data.

---
+++ ChurchSermonsMetaData/Data/SeriesStorage.cs
@@ -0,0 +1,44 @@
+﻿
+using global::ChurchSermonsMetaData.Models;
+using System.Text.Json;
+
+namespace ChurchSermonsMetaData.Data;
+
+internal class SeriesStorage
+{
+    private static readonly JsonSerializerOptions CachedJsonOptions = new() { WriteIndented = true };
+
+    private Series Series { get; set; } = new Series() { SeriesNames= ["One-Off"] };
+    public static string AppFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ChurchSermonsMetaData");
+
+    public static string DataFile => Path.Combine(AppFolder, "series.json");
+
+    public void AddSeries(string elder) => Series.SeriesNames.Add(elder);
+
+    public Series LoadOptions()
+    {
+        if (!File.Exists(DataFile))
+        {
+            Directory.CreateDirectory(AppFolder);
+            File.WriteAllText(DataFile, JsonSerializer.Serialize(Series, CachedJsonOptions));
+            return Series;
+        }
+
+        string json = File.ReadAllText(DataFile);
+        Series = JsonSerializer.Deserialize<Series>(json, CachedJsonOptions)!;
+        return Series;
+    }
+
+    public void SaveOptions()
+    {
+        File.WriteAllText(DataFile, JsonSerializer.Serialize(Series, CachedJsonOptions));
+        OnOptionsSaved();
+    }
+
+    public static event EventHandler? OptionsSaved;
+
+    protected static void OnOptionsSaved()
+    {
+        OptionsSaved?.Invoke(null, EventArgs.Empty);
+    }
+}
---
+++ ChurchSermonsMetaData/Data/SpeakerStorage.cs
@@ -0,0 +1,37 @@
+﻿using ChurchSermonsMetaData.Models;
+using System.Text.Json;
+
+namespace ChurchSermonsMetaData.Data;
+
+internal class SpeakerStorage
+{
+    private static readonly JsonSerializerOptions CachedJsonOptions = new() { WriteIndented = true };
+
+    private Speakers Speakers { get; set; } = new Speakers();
+    public static string AppFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ChurchSermonsMetaData");
+
+    public static string DataFile => Path.Combine(AppFolder, "speakers.json");
+
+    public void AddElder(string elder) => Speakers.ElderList.Add(elder);
+    public void AddSpeaker(string speaker) => Speakers.PreviousSpeakers.Add(speaker);
+
+    public Speakers LoadOptions()
+    {
+        if (!File.Exists(DataFile))
+        {
+            Directory.CreateDirectory(AppFolder);
+            File.WriteAllText(DataFile, JsonSerializer.Serialize(Speakers, CachedJsonOptions));
+            //Speakers = options;
+            return Speakers;
+        }
+
+        string json = File.ReadAllText(DataFile);
+        Speakers = JsonSerializer.Deserialize<Speakers>(json, CachedJsonOptions)!;
+        return Speakers;
+    }
+
+    public void SaveOptions()
+    {
+        File.WriteAllText(DataFile, JsonSerializer.Serialize(Speakers, CachedJsonOptions));
+    }
+}
