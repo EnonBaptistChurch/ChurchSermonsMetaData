f48f6727f2973378e220708cf81163f48465976a
David <davidmitchellfootball@googlemail.com>
2025-11-22 20:52:43 +0000

    add sermon metadata from Transcript?

---
+++ SermonData/Models/SermonMetaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SermonData.Models
+{
+    public class SermonMetaData
+    {
+        public string Title { get; set; }
+        public string Series { get; set; }
+        public List<string> BiblePassage { get; set; }
+    }
+}
